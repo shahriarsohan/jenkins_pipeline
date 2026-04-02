e1a470a9ead96a212e4675c213659656ba2ab965
Tarek Akik Sohan <sohanmock@gmail.com>
2026-04-03 00:06:53 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def health_check():
-    return {"status": "breathing... and healthy v2"}
+    return {"status": "breathing... and healthy v3"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)

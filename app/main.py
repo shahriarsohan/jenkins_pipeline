81a0a26f607bf6976d30580a2555e6f7ba41a548
shahriarsohan <sohanmock@gmail.com>
2026-04-07 14:51:10 +0600

    Fixing empty commit message

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def health_check():
-    return {"status": "healthy v10"}
+    return {"status": "healthy v11"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)

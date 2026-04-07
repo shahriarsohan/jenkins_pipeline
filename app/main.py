7dce3a7a9af38abb501eea78a6f9c9657410c19c
shahriarsohan <sohanmock@gmail.com>
2026-04-07 14:17:03 +0600

    Jenkinsfile updated

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def health_check():
-    return {"status": "healthy v6  new"}
+    return {"status": "healthy v7"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
